67e4a9f3c4a1777b960d2cb76107850e20e29248
Erwan MORIO <morioe2809@gmail.com>
2023-09-13 17:11:45 +0200

    add configModule to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
@@ -6,7 +8,13 @@ import { UserModule } from './user/user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [UserModule, AuthenticationModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'environments/.env',
+    }),
+    UserModule,
+    AuthenticationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
