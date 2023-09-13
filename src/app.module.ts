7282ba9e35283f2a723de8978d610be7b240398c
VincentProuchet <prouchetvincent@hotmail.com>
2023-09-13 22:06:39 +0200

    Reparation d'import
    certains imports malformé par l'IDE empêchaient la transpilation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './user/user.module';
+
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UsersModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,26 +2,26 @@ import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post,
 import { Request, Response } from 'express';
 
 import { UserService } from './user.service';
-import { EmailValidationPipe } from 'src/common/pipe/email-validation.pipe';
+import { EmailValidationPipe } from '../common/pipe/email-validation.pipe';
 
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
 
-  @Get('')
-  async findAll(): Promise<any[]> {
-    return await this.userService.getAll();
-  }
+    @Get('')
+    async findAll(): Promise<any[]> {
+        return await this.userService.getAll();
+    }
 
-  @Get('/byId/:id')
-  async findById(@Param('id', ParseIntPipe) userId: number): Promise<any> {
-    return await this.userService.getById(userId);
-  }
+    @Get('/byId/:id')
+    async findById(@Param('id', ParseIntPipe) userId: number): Promise<any> {
+        return await this.userService.getById(userId);
+    }
 
-  @Post('/byEmail')
-  @HttpCode(200)
-  async findByEmail(@Body('email', EmailValidationPipe) userEmail: string): Promise<any> {
-    return await this.userService.getByEmail(userEmail);
-  }
+    @Post('/byEmail')
+    @HttpCode(200)
+    async findByEmail(@Body('email', EmailValidationPipe) userEmail: string): Promise<any> {
+        return await this.userService.getByEmail(userEmail);
+    }
 }
