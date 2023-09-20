f507c3888a980fe598afb032b4386732cc42f26a
Erwan MORIO <morioe2809@gmail.com>
2023-09-20 14:06:12 +0200

    add authenticationController test

--- src/authentication/authentication.controller.spec.ts
+++ src/authentication/authentication.controller.spec.ts
@@ -1,19 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AuthenticationController } from './authentication.controller';
+import { AuthenticationService } from './authentication.service';
+import { UserGetDto } from 'src/user/dto/user-get.dto';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { AccessTokenDto } from './dto/access-token.dto';
+
+
+const accessToken: AccessTokenDto = {
+  access_token: 'test_token_123'
+};
 
 describe('AuthenticationController', () => {
-  let controller: AuthenticationController;
+  let authController: AuthenticationController;
+  let authService: AuthenticationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthenticationController],
+      providers: [
+        {
+          provide: AuthenticationService,
+          useValue: {
+            register: jest.fn().mockImplementation(
+              (dto: RegisterDto) => {
+                return Promise.resolve<UserGetDto>({
+                  id: 1,
+                  email: dto.email
+                });
+              }
+            ),
+            login: jest.fn().mockResolvedValue(accessToken)
+          }
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthenticationController>(AuthenticationController);
+    authController = module.get<AuthenticationController>(AuthenticationController);
+    authService = module.get<AuthenticationService>(AuthenticationService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('signUp', () => {
+    it('should return the newly created user as UserDtoGet', async () => {
+      const registerDto: RegisterDto = {
+        email: 'foo@bar.com',
+        password: 'foobar1234'
+      }
+      const userDto: UserGetDto = {
+        id: 1,
+        email: 'foo@bar.com'
+      };
+
+      expect(await authController.signUp(registerDto)).toStrictEqual<UserGetDto>(userDto);
+    });
+  });
+
+  describe('signIn', () => {
+    it('should return an access_token', async () => {
+      const loginDto: LoginDto = {
+        email: 'foo@bar.com',
+        password: 'foobar1234'
+      };
+
+      expect(await authController.signIn(loginDto)).toBe<AccessTokenDto>(accessToken);
+    });
   });
 });
